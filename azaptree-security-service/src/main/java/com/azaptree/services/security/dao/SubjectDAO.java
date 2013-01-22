0c803901189c944589d8deb63dc70f6fbd7a8f5e
Alfio Zappala <azappala@azaptree.com>
2013-01-22 05:50:33 -0500

    saving code

--- azaptree-security-service/src/main/java/com/azaptree/services/security/dao/SubjectDAO.java
+++ azaptree-security-service/src/main/java/com/azaptree/services/security/dao/SubjectDAO.java
@@ -3,6 +3,8 @@ package com.azaptree.services.security.dao;
 import java.util.UUID;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.azaptree.services.domain.entity.dao.JDBCEntityDAOSupport;
 import com.azaptree.services.domain.entity.dao.Page;
@@ -10,6 +12,7 @@ import com.azaptree.services.domain.entity.dao.SearchResults;
 import com.azaptree.services.domain.entity.dao.SortField;
 import com.azaptree.services.security.domain.Subject;
 
+@Repository
 public class SubjectDAO extends JDBCEntityDAOSupport<Subject> {
 
 	public SubjectDAO(final JdbcTemplate jdbc) {
@@ -17,36 +20,42 @@ public class SubjectDAO extends JDBCEntityDAOSupport<Subject> {
 	}
 
 	@Override
+	@Transactional
 	public UUID create(final Subject entity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public boolean delete(final UUID id) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
+	@Transactional
 	public SearchResults<Subject> findAll(final Page page, final SortField... sort) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public SearchResults<Subject> findByExample(final Subject example, final Page page, final SortField... sort) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public Subject findById(final UUID id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public void update(final Subject entity) {
 		// TODO Auto-generated method stub
 
